7d08f0568463c79eb173030cd29a42b93c2244e0
Matthias Rampke <matthias@rampke.de>
2010-08-24 01:14:01 +0200

    Helper functions for jumping back/forward (i.e., [ and ])

--- bf.hs
+++ bf.hs
@@ -6,3 +6,21 @@ data State = State {
     input :: [Char],
     output :: [Char]
     } deriving (Eq,Show)
+
+_jumpRight :: Integer -> State -> State
+_jumpRight level (State lts rts lcs ('[':rcs) i o) = _jumpRight (level+1) (State lts rts ('[':lcs) rcs i o)
+_jumpRight 0 state = state
+_jumpRight level (State lts rts lcs (']':rcs) i o) = _jumpRight (level-1) (State lts rts (']':lcs) rcs i o)
+_jumpRight level (State lts rts lcs (c:rcs) i o) = _jumpRight level (State lts rts (c:lcs) rcs i o)
+
+_jumpLeft :: Integer -> State -> State
+_jumpLeft level (State lts rts (']':lcs) rcs i o) = _jumpLeft (level+1) (State lts rts lcs (']':rcs) i o)
+_jumpLeft 0 state = state
+_jumpLeft level (State lts rts ('[':lcs) rcs i o) = _jumpLeft (level-1) (State lts rts lcs ('[':rcs) i o)
+_jumpLeft level (State lts rts (c:lcs) rcs i o) = _jumpLeft level (State lts rts lcs (c:rcs) i o)
+
+jumpRight :: State -> State
+jumpRight = _jumpRight 0
+
+jumpLeft :: State -> State
+jumpLeft = _jumpLeft 0
