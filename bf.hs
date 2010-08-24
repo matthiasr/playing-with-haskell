f7ac5d73bc5d03f921a538b3ae30a0333c244082
Matthias Rampke <matthias@rampke.de>
2010-08-24 03:11:22 +0200

    FIX wrong behaviour due to false shortcut in jump helpers

--- bf.hs
+++ bf.hs
@@ -8,19 +8,19 @@ data State = State {
     } deriving (Eq,Show)
 
 _jumpRight :: Integer -> State -> State
-_jumpRight level (State lts rts lcs ('[':rcs) i o) = _jumpRight (level+1) (State lts rts ('[':lcs) rcs i o)
 _jumpRight 0 state = state
+_jumpRight level (State lts rts lcs ('[':rcs) i o) = _jumpRight (level+1) (State lts rts ('[':lcs) rcs i o)
 _jumpRight level (State lts rts lcs (']':rcs) i o) = _jumpRight (level-1) (State lts rts (']':lcs) rcs i o)
 _jumpRight level (State lts rts lcs (c:rcs) i o) = _jumpRight level (State lts rts (c:lcs) rcs i o)
 
 _jumpLeft :: Integer -> State -> State
-_jumpLeft level (State lts rts (']':lcs) rcs i o) = _jumpLeft (level+1) (State lts rts lcs (']':rcs) i o)
 _jumpLeft 0 state = state
+_jumpLeft level (State lts rts (']':lcs) rcs i o) = _jumpLeft (level+1) (State lts rts lcs (']':rcs) i o)
 _jumpLeft level (State lts rts ('[':lcs) rcs i o) = _jumpLeft (level-1) (State lts rts lcs ('[':rcs) i o)
 _jumpLeft level (State lts rts (c:lcs) rcs i o) = _jumpLeft level (State lts rts lcs (c:rcs) i o)
 
 jumpRight :: State -> State
-jumpRight = _jumpRight 0
+jumpRight (State lts rts lcs (c:rcs) i o) = _jumpRight 1 (State lts rts (c:lcs) rcs i o)
 
 jumpLeft :: State -> State
-jumpLeft = _jumpLeft 0
+jumpLeft (State lts rts (c:lcs) rcs i o) = _jumpLeft 1 (State lts rts lcs (c:rcs) i o)
