4d20732fe7c6ae2e4855ed15c154ab73bf102171
Matthias Rampke <matthias@rampke.de>
2010-08-24 03:12:53 +0200

    Brainfuck implementation

--- bf.hs
+++ bf.hs
@@ -1,3 +1,5 @@
+import Char (ord,chr)
+
 data State = State {
     leftTape :: [Integer],
     rightTape :: [Integer],
@@ -24,3 +26,21 @@ jumpRight (State lts rts lcs (c:rcs) i o) = _jumpRight 1 (State lts rts (c:lcs) 
 
 jumpLeft :: State -> State
 jumpLeft (State lts rts (c:lcs) rcs i o) = _jumpLeft 1 (State lts rts lcs (c:rcs) i o)
+
+_bf :: State -> State
+_bf state@(State {rightCommands = []}) = state -- Termination: out of commands.
+_bf (State lt [] lc rc i o) = _bf (State lt [0] lc rc i o) -- needs more tape (right)!
+_bf (State [] rt lc rc i o) = _bf (State [0] rt lc rc i o) -- needs more tape (left)! 
+-- actual BrainFuck command implementation
+_bf (State (t:lt) rt lc ('<':rc) i o) = _bf (State lt (t:rt) ('<':lc) rc i o) -- <
+_bf (State lt (t:rt) lc ('>':rc) i o) = _bf (State (t:lt) rt ('>':lc) rc i o) -- >
+_bf (State lt (t:rt) lc ('+':rc) i o) = _bf (State lt ((succ t):rt) ('+':lc) rc i o) -- +
+_bf (State lt (t:rt) lc ('-':rc) i o) = _bf (State lt ((pred t):rt) ('-':lc) rc i o) -- -
+_bf state@(State lt (0:rt) lc ('[':rc) i o) = _bf (jumpRight state) -- [
+_bf state@(State lt (t:rt) lc (']':rc) i o) | t /= 0 = _bf (jumpLeft state) -- ]
+_bf (State lt (t:rt) lc ('.':rc) i o) = _bf (State lt (t:rt) ('.':lc) rc i ((chr (fromInteger t)):o)) -- .
+_bf (State lt (_:rt) lc (',':rc) (i:is) o) = _bf (State lt ((toInteger (ord i)):rt) (',':lc) rc is o) -- ,
+_bf (State lt rt lc (c:rc) i o) = _bf (State lt rt (c:lc) rc i o) -- ignore everything else
+
+bf :: String -> String -> String
+bf commands input = reverse (output (_bf (State [] [] "" commands input "")))
